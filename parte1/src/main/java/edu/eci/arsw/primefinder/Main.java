cbb5a159b648fdf90fcbba4505ff9e2156fc0f7c
Ana Gabriela Silva <ana.silva-b@mail.escuelaing.edu.co>
2021-08-19 15:21:27 -0500

    PARTE I (1-2)

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		PrimeFinderThread pft=new PrimeFinderThread(0, 9999999);
+		PrimeFinderThread t2=new PrimeFinderThread(10000000, 19999999);
+		PrimeFinderThread t3=new PrimeFinderThread(20000000, 30000000);
 		
 		pft.start();
+		t2.start();
+		t3.start();
 		
 		
 	}
