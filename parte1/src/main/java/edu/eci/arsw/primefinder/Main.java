c19041ec819d56003744047175fd277ec2c7b56e
Ana Gabriela Silva <ana.silva-b@mail.escuelaing.edu.co>
2021-08-19 19:53:23 -0500

    Primera parte completa

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,18 +1,59 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+
+		String in;
+        Scanner scanner;
+        Date startDate, endDate;
+		int seconds = 0;
+		startDate = new Date();
+		LinkedList<PrimeFinderThread> threads = new LinkedList<>();
 		
 		PrimeFinderThread pft=new PrimeFinderThread(0, 9999999);
 		PrimeFinderThread t2=new PrimeFinderThread(10000000, 19999999);
 		PrimeFinderThread t3=new PrimeFinderThread(20000000, 30000000);
+
+		threads.add(pft);
+		threads.add(t2);
+		threads.add(t3);
+
+		for(PrimeFinderThread thread:threads){
+            thread.start();
+        }
 		
-		pft.start();
-		t2.start();
-		t3.start();
-		
-		
-	}
-	
+		while(seconds!=5) {
+            endDate = new Date();
+            seconds = (int)((endDate.getTime()-startDate.getTime())/1000);
+        }
+
+        for(PrimeFinderThread thread:threads) {
+            thread.setSuspend(true);
+        }
+
+        System.out.println("Presione ENTER para continuar");
+
+        scanner = new Scanner(System.in);
+        in = scanner.nextLine();
+
+        if(in.equals("")) {
+            for(PrimeFinderThread thread:threads) {
+                thread.resumeThread();
+            }
+        }
+    }
+
 }
+
+	
+
+
+
+
+	
+
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -7,7 +7,7 @@ public class PrimeFinderThread extends Thread{
 
 	
 	int a,b;
-	
+	boolean stop;
 	private List<Integer> primes=new LinkedList<Integer>();
 	
 	public PrimeFinderThread(int a, int b) {
@@ -17,15 +17,23 @@ public class PrimeFinderThread extends Thread{
 	}
 
 	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				System.out.println(i);
-			}
-		}
-		
-		
-	}
+        for (int i=a;i<=b;i++){
+            if (isPrime(i)){
+                primes.add(i);
+                System.out.println(i);
+            }
+
+            synchronized(this) {
+                while(stop) {
+                    try {
+                        this.wait();
+                    } catch(InterruptedException e) {
+                        System.out.println("Erro in the Process");
+                    }
+                }
+            }
+        }
+    }
 	
 	boolean isPrime(int n) {
 	    if (n%2==0) return false;
@@ -39,8 +47,18 @@ public class PrimeFinderThread extends Thread{
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
-	
+
+	synchronized void resumeThread() {
+        stop = false;
+        notify();
+    }
+
+    public void setSuspend(boolean stop) {
+        this.stop = stop;
+    }
 }
+	
+	
+	
+	
+
